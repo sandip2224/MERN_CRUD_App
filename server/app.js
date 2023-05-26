33c997d46a7ed852ea7b026e5e944771e941c3f1
sandip2224 <sandipan2224@gmail.com>
2023-05-26 13:35:06 +0530

    app file server

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config({ path: './.env' })
+
+const connectDB = require('./helpers/db')
+
+// MongoDB Connection Init
+connectDB(process.env.MONGO_URI)
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/api/students', require('./routes/student.route'))
+
+// PORT
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, console.log('🚀 Server up and running on port ' + PORT))
+
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  if (!err.statusCode) err.statusCode = 500;
+  res.status(err.statusCode).json({
+    msg: err?.message || 'Something went wrong!'
+  });
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Server Error: ${err.message}`)
+
+  server.close(() => process.exit(1))
+})
