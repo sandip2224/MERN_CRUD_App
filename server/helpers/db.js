3f0ad3b6f9dd449028651aa6dedb45f93718ed0f
sandip2224 <sandipan2224@gmail.com>
2023-05-26 13:35:30 +0530

    database connectivity

---
+++ server/helpers/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const connectDB = async (val) => {
+  try {
+    const conn = await mongoose.connect(val, { useNewUrlParser: true, useUnifiedTopology: true })
+
+    console.log(`✔ MongoDB Cluster Connected: ${conn.connection.host}!`)
+  }
+  catch (err) {
+    console.log('🟥 Error in db connection: ' + err)
+  }
+}
+
+module.exports = connectDB
